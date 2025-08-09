09cffae5dfbd294d54c955f37494024c58f7f75b
Rishabh Shah <26rkshah@gmail.com>
2025-08-09 14:24:22 -0700

    update chat to format chat messages. add hard coded chat messages for testing

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,15 +1,33 @@
-import { useParams } from "react-router-dom";
+// import { useAuth } from "../hooks/useAuth"; // UNCOMMENT AFTER TESTING AUTHENTICATION
+import InputBox from "../components/InputBox";
+import ChatBubble from "../components/ChatBubble";
+import "./Chat.css";
 
 export default function Chat() {
-    
-    const { id } = useParams();
 
-    console.log("Chat component rendered");
-    console.log(id);
+    /** DEV: COMMENTED OUT THE FOLLOWING TO ALLOW FOR TESTING WHILE AUTHENTICATIOn
+     * IS NOT IMPLEMENTED
+
+    const { user, logout } = useAuth();
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+        } catch (error) {
+            console.error("Error logging out:", error);
+        }
+    };
+
+    */
 
     return (
-        <div>
-            <h1>Chat {id}</h1>
-        </div>
+        <>
+            <div className="chat-messages">
+                <ChatBubble message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." sender="assistant" />
+                <ChatBubble message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." sender="user" />
+                <ChatBubble message="Here is some math to do. $$\frac{3}{4} + \frac{5}{6}$$" sender="user" />
+            </div>
+            <InputBox />
+        </>
     );
 }
