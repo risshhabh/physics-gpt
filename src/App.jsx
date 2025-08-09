9b1d3a4ee014ace4c638c4c6e3be71a92167358a
Rishabh Shah <26rkshah@gmail.com>
2025-08-08 22:01:53 -0700

    commented out protectedroute function to allow for navigation while not logged in during development

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ import ScrollToTop from "./components/ScrollToTop";
 
 // ensure the user cannot navigate to unwanted pages if not logged in
 function ProtectedRoute({ children }) {
-    const { user } = useAuth();
+    // const { user } = useAuth();
     
-    if (!user) return <Navigate to="/auth" />;
+    // if (!user) return <Navigate to="/auth" />;
     
     return children;
 }
