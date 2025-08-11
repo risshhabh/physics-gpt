6a92a9dbf133d0b5382d38d73915f3f8c58e00c9
Rishabh Shah <26rkshah@gmail.com>
2025-08-10 21:09:23 -0700

    logout button implementation

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,14 +1,11 @@
-// import { useAuth } from "../hooks/useAuth"; // UNCOMMENT AFTER TESTING AUTHENTICATION
+import { useAuth } from "../hooks/useAuth";
+import { LogOut } from "lucide-react";
 import InputBox from "../components/InputBox";
 import ChatBubble from "../components/ChatBubble";
 import "./Chat.css";
 
 export default function Chat() {
-
-    /** DEV: COMMENTED OUT THE FOLLOWING TO ALLOW FOR TESTING WHILE AUTHENTICATIOn
-     * IS NOT IMPLEMENTED
-
-    const { user, logout } = useAuth();
+    const { logout } = useAuth();
 
     const handleLogout = async () => {
         try {
@@ -18,15 +15,21 @@ export default function Chat() {
         }
     };
 
-    */
-
     return (
         <>
+            <button
+                className="logout-button-fixed"
+                onClick={handleLogout}
+            >
+                <LogOut size={20} />
+            </button>
+
             <div className="chat-messages">
                 <ChatBubble message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." sender="assistant" />
                 <ChatBubble message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." sender="user" />
                 <ChatBubble message="Here is some math to do. $$\frac{3}{4} + \frac{5}{6}$$" sender="user" />
             </div>
+            
             <InputBox />
         </>
     );
